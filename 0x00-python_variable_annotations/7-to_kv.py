cc77affda8114d31b48b97b61f3d3bfec71a9193
diasbouk <boukdsaid@gmail.com>
2024-10-11 15:29:21 +0100

    Task 7 - Tuple typing

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+7-to_kv.py
+"""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+    """ Returns a tuple with k, (v)2"""
+    return (k, v * v)
