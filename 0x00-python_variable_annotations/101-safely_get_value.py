483bb0f2cd4b8936e36d6e10956920a5c41c6eb0
diasbouk <boukdsaid@gmail.com>
2024-10-11 22:16:38 +0100

    TypeVar

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -10,8 +10,9 @@ from typing import TypeVar, Mapping, Union, Any
 
 ty = TypeVar('ty')
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[ty, None]
-                      = None) -> Union[ty, Any]:
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[ty, None] = None) -> Union[ty, Any]:
     """ TypeVar """
     if key in dct:
         return dct[key]
