553861e39c903c7f51e40b201c2b433c9a98556a
diasbouk <boukdsaid@gmail.com>
2024-10-11 15:24:05 +0100

    Task 6 - using Union instead of | for double types

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,10 +4,10 @@
 6-sum_mixed_list
 """
 
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[int | float]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """ Sums a mixte """
     tot: float = 0
     for i in mxd_list:
