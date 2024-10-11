591490e39972b85d14e861252eff9107165579e0
diasbouk <boukdsaid@gmail.com>
2024-10-11 15:19:30 +0100

    Same as prev task , Using List from typing module for Type Annotation

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+from typing import List
+
 """
 6-sum_mixed_list
 """
 
 
-def sum_mixed_list(mxd_list: list[int | float]) -> float:
+def sum_mixed_list(mxd_list: List[int | float]) -> float:
     """ Sums a mixte """
     tot: float = 0
     for i in mxd_list:
