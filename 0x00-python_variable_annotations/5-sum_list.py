ee65e07635be86f3cb7acb8db10d8335cc177a03
diasbouk <boukdsaid@gmail.com>
2024-10-11 12:27:02 +0100

    Task 5 - Typed funcs . floats sum

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+5-sum_list
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """ Sums the floats """
+    total: float = 0.0
+    for ele in input_list:
+        total += ele
+    return total
