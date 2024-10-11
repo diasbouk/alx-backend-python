33484c0e0f72ae5055e8b903e5ed547845048b66
diasbouk <boukdsaid@gmail.com>
2024-10-11 15:18:12 +0100

    Using List for type annotation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+from typing import List
 
 """
 5-sum_list
 """
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ Sums the floats """
     total: float = 0
     for ele in input_list:
