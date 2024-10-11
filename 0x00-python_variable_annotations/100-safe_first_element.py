de34fdcd38561fa49a724102a4576f93ce96ee8d
diasbouk <boukdsaid@gmail.com>
2024-10-11 15:50:25 +0100

    Sequence[Any] -> Union[Any, None]

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,11 +5,11 @@
 """
 
 
-# The types of the elements of the input are not know
 from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ The types of the elements of the input are not know """
     if lst:
         return lst[0]
     else:
